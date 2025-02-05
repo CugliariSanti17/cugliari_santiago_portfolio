9d7e2547b69d5091e3a4bfa6748b8f36e980a045
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-02-05 17:48:14 -0300

    cambios en la configuracion para deploy

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -84,8 +84,8 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+    'default': dj_database_url(
+        default='sqlite:///db.sqlite3',
         conn_max_age=600
     )
 }
