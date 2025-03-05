966d4f7460ad9dde3b4e95389b69e3cbd4c04ff4
CugliariSanti17 <cugliarisantiago@gmail.com>
2025-03-05 14:18:49 -0300

    cambios en la variable database_url

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -82,15 +82,13 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 print (DATABASE_URL)
 
 DATABASES = {
     'default': dj_database_url.config(default=DATABASE_URL)
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
